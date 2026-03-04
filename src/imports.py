859d3262e7918d7b922a8ec5d51da9d37addb2f4
theoclo <clothilde.molinatti@gmail.com>
2026-03-04 13:02:23 +0100

    Refactor imports in section1.py by consolidating them into imports.py

---
+++ src/imports.py
@@ -0,0 +1,12 @@
+import os
+from PIL import Image
+
+import numpy as np
+import torchvision
+import pickle
+
+import torch
+import torch.nn.parallel
+import torch.utils.data
+import torchvision.models as models
+import matplotlib.pyplot as plt
--- src/section1.py
+++ src/section1.py
@@ -1,15 +1,4 @@
-import os
-from PIL import Image
-
-import numpy as np
-import torchvision
-import pickle
-
-import torch
-import torch.nn.parallel
-import torch.utils.data
-import torchvision.models as models
-import matplotlib.pyplot as plt
+from imports import *
 
 def s1(nameim):
     # 1. Garder l'image originale pour l'affichage final
